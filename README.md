bd65e14ff18f668833d3b0acace323c84b697e36
Christoph Jean-Luc Schneider <kopilot.schneider@protonmail.com>
2022-10-22 20:17:22 +0200

    run cargo fmt

--- README.md
+++ README.md
@@ -10,4 +10,5 @@ Infinite Game of Life is published under the [MIT License](./LICENSE).
 
 ## Icon
 
-Icon copied and modified from [tabler-icons](https://tabler-icons.io/i/chart-grid-dots) which is published under the [MIT License](https://github.com/tabler/tabler-icons/blob/master/LICENSE).
+Icon copied and modified from [tabler-icons](https://tabler-icons.io/i/chart-grid-dots)
+which is published under the [MIT License](https://github.com/tabler/tabler-icons/blob/master/LICENSE).
--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,19 @@ use infinite_game_of_life::app::*;
 use rgb::*;
 
 fn main() {
-    let image = lodepng::decode32_file("assets/chart-grid-dots.png").unwrap();
-    let icon = IconData {
-        rgba: image.buffer.as_bytes().to_vec(),
-        width: image.width as u32,
-        height: image.height as u32,
-    };
-    let options = NativeOptions {
-        icon_data: Some(icon),
-        ..Default::default()
-    };
-    eframe::run_native(
-        "Infinite Game of Life",
-        options,
-        Box::new(|_cc| Box::new(App::default())),
-    );
+	let image = lodepng::decode32_file("assets/chart-grid-dots.png").unwrap();
+	let icon = IconData {
+		rgba: image.buffer.as_bytes().to_vec(),
+		width: image.width as u32,
+		height: image.height as u32,
+	};
+	let options = NativeOptions {
+		icon_data: Some(icon),
+		..Default::default()
+	};
+	eframe::run_native(
+		"Infinite Game of Life",
+		options,
+		Box::new(|_cc| Box::new(App::default())),
+	);
 }
