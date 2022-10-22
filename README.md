a8d5315f6c2007e168cf8da614ccda0afa8221d1
Christoph Jean-Luc Schneider <kopilot.schneider@protonmail.com>
2022-10-22 20:16:03 +0200

    init project

---
+++ README.md
@@ -0,0 +1,13 @@
+# Infinite Game of Life
+
+```sh
+$ cargo run
+```
+
+## License
+
+Infinite Game of Life is published under the [MIT License](./LICENSE).
+
+## Icon
+
+Icon copied and modified from [tabler-icons](https://tabler-icons.io/i/chart-grid-dots) which is published under the [MIT License](https://github.com/tabler/tabler-icons/blob/master/LICENSE).
---
+++ src/main.rs
@@ -0,0 +1,23 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+use eframe::{IconData, NativeOptions};
+use infinite_game_of_life::app::*;
+use rgb::*;
+
+fn main() {
+    let image = lodepng::decode32_file("assets/chart-grid-dots.png").unwrap();
+    let icon = IconData {
+        rgba: image.buffer.as_bytes().to_vec(),
+        width: image.width as u32,
+        height: image.height as u32,
+    };
+    let options = NativeOptions {
+        icon_data: Some(icon),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Infinite Game of Life",
+        options,
+        Box::new(|_cc| Box::new(App::default())),
+    );
+}
